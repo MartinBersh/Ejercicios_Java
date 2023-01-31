af4585ce3df8c7817d74457dea3ceab5792119a8
martin <118768668+MartinBersh@users.noreply.github.com>
2023-01-31 15:26:44 -0500

    commit

---
+++ src/Ejer6/Main6.java
@@ -0,0 +1,19 @@
+package Ejer6;
+
+public class Main6 {
+
+    public static void main(String[] args) {
+        AreaRectangulo areaRectangulo = new AreaRectangulo();
+
+        areaRectangulo.altura = 12;
+        areaRectangulo.base = 20;
+
+        System.out.println(areaRectangulo.validarDatos());
+
+        System.out.println("El area del rectgangulo es de: " + areaRectangulo.calcularArea() + "cm");
+
+
+
+    }
+
+}
